23c91328f73d7da0e99748c280254cdd21c6e521
heeouo <myosotis248@gmail.com>
2025-05-27 19:12:14 +0900

    web hosting test - edit link connection

--- Start/start.js
+++ Start/start.js
@@ -1,8 +1,8 @@
-document.getElementById('tutorialCard').addEventListener('click', () => {
-    window.location.href = 'https://heeouo.github.io/open-daw-melto/Tutorial-temp/tutorial.html';
+document.getElementById('Tutorial').addEventListener('click', () => {
+    window.location.href = 'https://heeouo.github.io/open-daw-melto/Tutorial-temp/index.html';
 });
 
-document.getElementById('tutorialCard').addEventListener('click', () => {
+document.getElementById('Daw').addEventListener('click', () => {
     window.location.href = 'https://heeouo.github.io/open-daw-melto/Daw-temp/daw.html';
 });
 
