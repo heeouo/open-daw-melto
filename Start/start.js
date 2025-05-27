218a0ed95496354eee22c31e4763d21f7622b424
heeouo <myosotis248@gmail.com>
2025-05-27 19:01:59 +0900

    튜토리얼 &daw화면 (미완)

---
+++ Start/start.js
@@ -0,0 +1,8 @@
+document.getElementById('tutorialCard').addEventListener('click', () => {
+    window.location.href = 'https://heeouo.github.io/open-daw-melto/Tutorial-temp/tutorial.html';
+});
+
+document.getElementById('tutorialCard').addEventListener('click', () => {
+    window.location.href = 'https://heeouo.github.io/open-daw-melto/Daw-temp/daw.html';
+});
+
