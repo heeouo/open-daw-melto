d758b619b066ea923e6cdc365ce4f1907d508e47
heeouo <myosotis248@gmail.com>
2025-06-04 22:35:09 +0900

    Fix: Tutorial root

--- Start/start.js
+++ Start/start.js
@@ -1,5 +1,5 @@
 document.getElementById('Tutorial').addEventListener('click', () => {
-    window.location.href = 'https://heeouo.github.io/open-daw-melto-Daw/';
+    window.location.href = 'https://heeouo.github.io/open-daw-melto-Tutorial/';
 });
 
 document.getElementById('Daw').addEventListener('click', () => {
